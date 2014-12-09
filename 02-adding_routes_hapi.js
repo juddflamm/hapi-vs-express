3f154c915b3e81828c79db2c0577ac5ee7c91ddb
Judd Flamm <kd7yva@gmail.com>
2014-12-09 15:29:49 -0700

    Updated added routes to serve static files also

--- 02-adding_routes_hapi.js
+++ 02-adding_routes_hapi.js
@@ -1,13 +1,24 @@
 var Hapi = require('hapi');
 var server = new Hapi.Server(3000);
 
-server.route({
-  method: 'GET',
-  path: '/',
-  handler: function (request, reply) {
-    reply('Hello World from Hapi');
+var publicFilesPath = (process.env.PUB_FILES)?process.env.PUB_FILES:'./public';
+
+server.route([
+  {
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+      reply('Hello World from Hapi');
+    }
+  },
+  {
+    method: 'GET',
+    path: '/public/{path*}',
+    handler: {
+      directory: { path: publicFilesPath, listing: false, index: true }
+    }
   }
-});
+]);
 
 server.start(function () {
   console.log('Hapi server running at:', server.info.uri);
